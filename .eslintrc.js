50bb1617f3716da190a7ac2278ddeea4d3af4fff
zhangjiachen <zhangjiachen@ctfo.com>
2018-06-29 14:56:18 +0800

    init modify

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 2017,
+        "sourceType": "module"
+    },
+    "rules": {
+        "semi": ["error", "always"],
+        "quotes": ["error", "double"]
+    }
+};
