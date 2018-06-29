1073b09af8a420a9b4b83ac638280ab663eca38d
zhangjiachen <zhangjiachen@ctfo.com>
2018-06-29 10:24:40 +0800

    init

---
+++ src/page/index.vue
@@ -0,0 +1,51 @@
+
+<template>
+  <div class="hello">
+
+    <mt-header title="标题过长会隐藏后面的内容啊哈哈哈哈">
+      <router-link to="/" slot="left">
+        <mt-button icon="back">返回</mt-button>
+      </router-link>
+      <mt-button icon="more" slot="right"></mt-button>
+    </mt-header>
+
+    <h1>{{ msg }} <span>{{msg}}</span></h1>
+    <h2>Essential Links</h2>
+    <h2>Ecosystem</h2>
+    <p>{{msg}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+h1{
+  color:red;
+  span{
+    color:blue;
+  }
+}
+</style>
